07574ef9f05f6af1241ed633054fdfd2e177367a
Andy Gill <andygill@ku.edu>
2020-06-14 17:15:31 -0500

    Using the world matrix of the projector

--- src/components/projectors.js
+++ src/components/projectors.js
@@ -10,6 +10,7 @@ AFRAME.registerComponent('srl-projection-material', {
   init: function () {
     this.src = null;
     this.projectorWorldPosition = new THREE.Vector3();
+    this.count = 0;
     console.log('data',this.data);
     this.el.setAttribute('material',
 			 { shader: "srl-projection-material"
@@ -32,6 +33,31 @@ AFRAME.registerComponent('srl-projection-material', {
     }
     projector.object3D.updateMatrixWorld();
     this.projectorWorldPosition.setFromMatrixPosition(projector.object3D.matrixWorld);
+    let m = new THREE.Matrix4();
+    //m = projector.object3D.matrixWorld.clone();
+    m.getInverse(projector.object3D.matrixWorld);
+    if (this.count++ % 1000 == 10) {
+      console.log('mat4',m);
+    }
+    let e = m.elements;
+    // may want to directly update material.uniforms?
+    this.el.setAttribute('material', 'col1', new THREE.Vector4(e[0],e[1],e[2],0))
+    this.el.setAttribute('material', 'col2', new THREE.Vector4(e[4],e[5],e[6],0))
+    this.el.setAttribute('material', 'col3', new THREE.Vector4(e[8],e[9],e[10],0))
+    this.el.setAttribute('material', 'col4', new THREE.Vector4(e[12],e[13],e[14],1))
+//			 { x: 1, y:0, z:0, w:0 })
+    
+
+    //			   col2: { x: 0, y:1, z:0, w:0 },
+//			   col3: { x: 0, y:0, z:1, w:0 },
+//			   col4: { x: 0, y:-1, z:2, w:1 }
+//			 { col1: { x: e[0],  y: e[1],  z:e[2],  w: e[3] },
+//			   col2: { x: e[4],  y: e[5],  z:e[6],  w: e[7] },
+//			   col3: { x: e[8],  y: e[9],  z:e[10], w: e[11] },
+//			   col4: { x: e[12], y: e[13], z:e[14], w: e[15] }
+//			 })
+			   
+    
 //    console.log('tick',this.projectorWorldPosition,this.el.getAttribute('material').projector);
     // For some reason, passing in the THREE.Vector3 direcly does not work.
     this.el.setAttribute('material', 'projector',
--- src/shaders/projection-material.js
+++ src/shaders/projection-material.js
@@ -8,6 +8,10 @@ AFRAME.registerShader('srl-projection-material', {
     projector: { type: 'vec3', is: 'uniform' },
     maxPhi:    { type: 'number', is: 'uniform' },
     maxTheta:  { type: 'number', is: 'uniform' },
+    col1:      { type: 'vec4', is: 'uniform' },
+    col2:      { type: 'vec4', is: 'uniform' },
+    col3:      { type: 'vec4', is: 'uniform' },
+    col4:      { type: 'vec4', is: 'uniform' }
   },
   
   // Can debug using document.querySelector('#mine').
@@ -19,15 +23,27 @@ varying vec2 pUV; // projected UV
 uniform vec3 projector;
 uniform float maxPhi;
 uniform float maxTheta;
+uniform vec4 col1;
+uniform vec4 col2;
+uniform vec4 col3;
+uniform vec4 col4;
 
 const float PI = 3.1415926535897932384626433832795;
 
+vec4 col1X = vec4(1.0,0.0,0.0,0.0);
+vec4 col2X = vec4(0.0,1.0,0.0,0.0);
+vec4 col3X = vec4(0.0,0.0,1.0,0.0);
+vec4 col4X = vec4(0.0,-1.0,2.0,1.0);
+
+mat4 projectorModelMatrix = mat4(col1,col2,col3,col4);
+
 void main() {
   vec4 theModel = modelMatrix * vec4( position, 1.0 ); 
-  vec3 theVec   = -(theModel.xyz - projector.xyz);
+  vec3 theVec   = (projectorModelMatrix  * theModel).xyz;
+//-(theModel.xyz - projector.xyz);
   float longitude = atan(theVec.x,theVec.z);
   float latitude = atan(theVec.y,length(theVec.xz));
-  pUV = 0.5 + vec2(longitude/(PI*maxPhi/180.0),(-latitude/(PI*maxTheta/180.0)));
+  pUV = 0.5 + vec2(longitude/(PI*maxPhi/180.0),(latitude/(PI*maxTheta/180.0)));
   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
 `,
