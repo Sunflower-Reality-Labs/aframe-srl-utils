8b9f215efdb0a9d42cd893329b4c9f8f11ab4376
Andy Gill <andygill@ku.edu>
2020-06-20 12:40:18 -0500

    left and right arms working in avator

--- src/components/avator.js
+++ src/components/avator.js
@@ -9,14 +9,16 @@ AFRAME.registerComponent('srl-avator', {
     leftShoulder: { type: 'selector' },
     leftElbow: { type: 'selector' },
     leftWrist: { type: 'selector' },
+    leftHand:  { type: 'selector', default: '#left-hand' },
     rightShoulder: { type: 'selector' },
     rightElbow: { type: 'selector' },
     rightWrist: { type: 'selector' },
+    rightHand:  { type: 'selector', default: '#right-hand' },
 
     // Dimensions of avator
     neckOffset: { type: 'vec3', default: { x: 0, y: -0.1, z: 0.05 }},
     shoulderOffset: { type: 'vec3', default: { x: -0.15, y: -0, z: 0.05 }},
-    upperArmLength : { type: 'number', default: 0.35 },
+    upperArmLength : { type: 'number', default: 0.30 },
     forearmLength : { type: 'number', default: 0.25 },
     handLength: { type: 'number', default: 0.10 },
 
@@ -87,86 +89,62 @@ AFRAME.registerComponent('srl-avator', {
       }
     }
 
-    let head = document.querySelector('#head');
-    head.object3D.setRotationFromQuaternion(reflectQ(eyesQ));
-    head.object3D.position.copy(reflectPos(eyesPos));
+    let head = this.data.head;
+    head && head.object3D.setRotationFromQuaternion(reflectQ(eyesQ));
+    head && head.object3D.position.copy(reflectPos(eyesPos));
     
     // Now, figure out where the (back of the) neck is
-    let neck = document.querySelector('#spine')
+    let neck = this.data.neck;
     let neckPos = new THREE.Vector3(0,-0.1,0.05);
     neckPos.applyQuaternion(eyesQ);
     neckPos.add(eyesPos);
-    neck.object3D.position.copy(reflectPos(neckPos));
+    neck && neck.object3D.position.copy(reflectPos(neckPos));
     // We do not do rotation on neck (yet)
-    
-    // Now, estimate the location of the shoulder
-    // Need to add rotation of shoulders
-    let shoulderPos = new THREE.Vector3(-0.15,0,-0.05);
-    shoulderPos.add(neckPos);
-    document.querySelector('#left-shoulder').object3D.position.copy(reflectPos(shoulderPos));
-    
-//    let shoulderQ = new THREE.Quaternion();
-//    shoulderQ.setFromEuler(new THREE.Euler(0,0,0));
-//    let 
 
-    let leftPos = new THREE.Vector3(1,2,3);
-    let leftHand = document.querySelector('#left-hand');
-//    poseMatrix.elements = pose.transform.matrix;
-    leftPos = leftHand.object3D.position;
-    leftPos = reflectPos(leftPos);    
-    document.querySelector('#left-wrist').object3D.position.copy(leftPos);
-    
-    // First, compute the elbow angle
-    let wristToShoulder = leftPos.distanceTo(shoulderPos);
-    let wristToElbow    = 0.35;
-    let elbowToShoulder = 0.35;
-    function cosineRule(a,b,c) {
-      return Math.acos(((b*b)+(c*c)-(a*a))/(2*b*c));
+    for (let side of ['left', 'right']) {
+      let reflectX = side == 'left' ? -1 : 1;
+
+      let shoulder = this.data[side + 'Shoulder'];
+      let elbow    = this.data[side + 'Elbow'];
+      let wrist    = this.data[side + 'Wrist'];
+      let hand     = this.data[side + 'Hand'];
+      
+      // Now, estimate the location of the shoulder
+      // Need to add rotation of shoulders
+      let shoulderPos = new THREE.Vector3(reflectX * 0.15,-0.10,-0.05);
+      shoulderPos.add(neckPos);
+      shoulder && shoulder.object3D.position.copy(reflectPos(shoulderPos));
+      
+      let handPos = hand.object3D.position;
+      wrist && wrist.object3D.position.copy(reflectPos(handPos));
+      
+      // First, compute the elbow angle
+      let wristToShoulder = handPos.distanceTo(shoulderPos);
+      let wristToElbow    = this.data.forearmLength + this.data.handLength;
+      let elbowToShoulder = this.data.upperArmLength;
+      const cosineRule = (a,b,c) => {
+	return Math.acos(((b*b)+(c*c)-(a*a))/(2*b*c)) || 0;
+      }
+      let h = cosineRule(wristToElbow,elbowToShoulder,wristToShoulder);
+      
+      // Now compute the y axis angle between the body and the wrist
+      let s2 = new THREE.Vector2(shoulderPos.x,shoulderPos.z);
+      let w2 = new THREE.Vector2(handPos.x,handPos.z);
+      let h2 = w2.clone().sub(s2).angle();
+      // Now, the rotation of the wrist from the vertical.
+      let t2 = handPos.clone();
+      t2.sub(shoulderPos);
+      t2.multiplyScalar(elbowToShoulder/wristToShoulder)
+      t2.applyEuler(new THREE.Euler(0,h2,0));
+      t2.applyEuler(new THREE.Euler(0,0,-h));
+      t2.applyEuler(new THREE.Euler(0,-h2,0));
+      t2.add(shoulderPos);
+      elbow && elbow.object3D.position.copy(reflectPos(t2));
+//      document.querySelector("#debug").object3D.position.set(0,h2,-1);
     }
-    let h = cosineRule(wristToElbow,elbowToShoulder,wristToShoulder);
-    if (isNaN(h)) {
-      h = 0;
-    }
-    // Debugging ball
-//     document.querySelector("#debug").object3D.position.set(0,h,-1);
-
-    // Now compute the y axis angle between the body and the wrist
-    let s2 = new THREE.Vector2(shoulderPos.x,shoulderPos.z);
-    let w2 = new THREE.Vector2(leftPos.x,leftPos.z);
-    let h2 = w2.clone().sub(s2).angle();
-    // Now, the rotation of the wrist from the vertical.
-    let t2 = leftPos.clone();
-    t2.sub(shoulderPos);
-    t2.multiplyScalar(elbowToShoulder/wristToShoulder)
-    t2.applyEuler(new THREE.Euler(0,h2,0));
-    t2.applyEuler(new THREE.Euler(0,0,-h));
-    t2.applyEuler(new THREE.Euler(0,-h2,0));
-    t2.add(shoulderPos);    
-    document.querySelector('#left-elbow').object3D.position.copy(t2);
-    document.querySelector("#debug").object3D.position.set(0,h2,-1);
-    
-
-    //    eyesPos = new THREE.Vector3(1,2,3);
-//    eyesPos.z = eyesPos.z - 1.0
-
-    //    head.object3D.setRotationFromEuler(new THREE.Euler(eyesRot.x,eyesRot.y,eyesRot.z));
-    
-    
+      
     this.c = (this.c || 0) + 1;
-//    head.object3D.matrix.elements = eyes.object3D.matrix.elements;
-    //    head.object3D.matrix.decompose(head.object3D.position, head.object3D.rotation, head.object3D.scale);
-//    let eyesPos = new THREE.Vector3(1,2,3);
-//    eyesPos.z = eyesPos.z + 0.5;
-//    let pos = head.object3D.position
-//    head.object3D.translateZ(-0.002);    
     if (this.c % 100 == 1 && false) {
-      head.object3D.matrix.elements = eyes.object3D.matrix.elements;
-      head.object3D.matrix.decompose(head.object3D.position, head.object3D.rotation, head.object3D.scale);
-      let pos = head.object3D.position
-      head.object3D.translateZ(-2);
-//	position.set({ x: pos.x, y: pos.y, z: pos.z})
-//      console.log('tick',head,headPos,headRot,myRot);
-//      head.object3D.set();
     }
   },
 
